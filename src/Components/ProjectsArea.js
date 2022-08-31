711c1fdecf8542d6ab16dc2b0d4204d45e01909e
Jared Fipps <jfipps1995@gmail.com>
2022-08-31 15:26:34 -0700

    Projects done

--- src/Components/ProjectsArea.js
+++ src/Components/ProjectsArea.js
@@ -6,10 +6,14 @@ import { BsDot } from "react-icons/bs";
 import Spoofy1 from "../Resources/Spoofy1.png";
 import Spoofy2 from "../Resources/Spoofy2.png";
 import Spoofy3 from "../Resources/Spoofy3.png";
+import NHL1 from "../Resources/NHL1.png";
+import NHL2 from "../Resources/NHL2.png";
+import NHL3 from "../Resources/NHL3.png";
 import "../CSS/LandingPage.css";
 
 function ProjectsArea() {
   const [index, setIndex] = useState(1);
+  const [activeProject, setActiveProject] = useState("Spoofy");
 
   const Next = () => {
     if (index !== 3) {
@@ -29,14 +33,38 @@ function ProjectsArea() {
     console.log(index);
   };
 
+  const ChangeActiveProject = (project) => {
+    setActiveProject(project);
+    setIndex(1);
+  };
+
   return (
     <>
       <div className="ProjectsArea">
         <Container>
-          <h1>Projects</h1>
+          <h1 className="ProjectsHeader">Projects</h1>
           <div className="ProjectCard">
-            <h2>Spoofy</h2>
-            <div className="CarouselDiv">
+            <div className="ProjectTitles">
+              <h2
+                onClick={() => ChangeActiveProject("Spoofy")}
+                className={
+                  activeProject === "Spoofy" ? "Selected" : "NotSelected"
+                }
+              >
+                Spoofy
+              </h2>
+              <h2
+                onClick={() => ChangeActiveProject("NHL")}
+                className={activeProject === "NHL" ? "Selected" : "NotSelected"}
+              >
+                NHLHistory
+              </h2>
+            </div>
+            <div
+              className={
+                activeProject === "Spoofy" ? "CarouselDiv" : "Inactive"
+              }
+            >
               <div className="Slider">
                 <GrFormPrevious
                   size={36}
@@ -53,6 +81,39 @@ function ProjectsArea() {
                 />
                 <img
                   src={Spoofy3}
+                  className={index == 3 ? "Visible" : "Invisible"}
+                />
+                <GrFormNext
+                  size={36}
+                  onClick={() => Next()}
+                  className="NaviArrow"
+                ></GrFormNext>
+              </div>
+              <div className="Dots">
+                <BsDot size={index == 1 ? 40 : 20}></BsDot>
+                <BsDot size={index == 2 ? 40 : 20}></BsDot>
+                <BsDot size={index == 3 ? 40 : 20}></BsDot>
+              </div>
+            </div>
+            <div
+              className={activeProject === "NHL" ? "CarouselDiv" : "Inactive"}
+            >
+              <div className="Slider">
+                <GrFormPrevious
+                  size={36}
+                  onClick={() => Prev()}
+                  className="NaviArrow"
+                ></GrFormPrevious>
+                <img
+                  src={NHL1}
+                  className={index == 1 ? "Visible" : "Invisible"}
+                />
+                <img
+                  src={NHL2}
+                  className={index == 2 ? "Visible" : "Invisible"}
+                />
+                <img
+                  src={NHL3}
                   className={index == 3 ? "Visible" : "Invisible"}
                 />
                 <GrFormNext
